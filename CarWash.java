1dd98b3e85c562eb5757a7dd0de3c5d06884cd3d
Niklasld <niklasld@gmail,com>
2018-03-19 11:36:01 +0100

    new class customer
    updated methods

--- CarWash.java
+++ CarWash.java
@@ -1,3 +1,9 @@
+import java.util.*;
+
 public class CarWash {
-   Woop
+   public static void main(String[] args) {
+      Customer Johnny = new Customer(350.00, 01);
+      
+      System.out.println("Your balance is: "+Johnny.getBalance()+" DKr.");
+   }
 }
---
+++ Customer.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Customer {
+   private double cardBalance = 0;
+   private int ID = 0;
+   
+   public Customer(double cardBalance, int ID) {
+      if(cardBalance>=200) {
+         this.cardBalance = cardBalance;
+         this.ID = ID;
+      }
+      else {
+         System.out.println("Must at least deposit 200 DKr.");
+      }     
+   }
+   
+   public void buyWash() {
+      //TODO
+   }
+   
+   public void rechargeWashcard() {
+      //TODO
+   }
+   
+   public double getBalance() {
+      
+      return cardBalance;
+   }
+}
